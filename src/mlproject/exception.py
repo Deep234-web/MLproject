2eeab96b592fb253822ff56a920fbea98f9abc4c
Deep <deeprajani1999@gmail.com>
2024-08-15 13:39:46 +0530

    solved exception error

--- src/mlproject/exception.py
+++ src/mlproject/exception.py
@@ -10,9 +10,9 @@ def error_message_detail(error,error_detail:sys):
     return error_message
 
 class CustomException(Exception):
-    def __init__(self,error_message,error_details:sys):
-        super().__init__(error_message)
-        self.error_message=error_message_detail(error_message,error_details)
+    def __init__(self,error,error_details:sys):
+        super().__init__(str(error))
+        self.error_message=error_message_detail(error,error_details)
 
     
     def __str__(self):
